3c219e031ab8ccce962a6dc777f8f7f0a786c403
weigan <1831197727@qq.com>
2023-07-05 23:52:24 +0800

    add objects and converters

---
+++ Speckle.Addin/Objects/Converters/ConverterSolidWorks/ConnectorSolidWorks.Speckle.cs
@@ -0,0 +1,23 @@
+﻿using Speckle.Core.Models;
+using System;
+using System.Collections.Generic;
+
+namespace Objects.Converter.SolidWorks;
+
+public partial class ConverterSolidWorks
+{
+    public bool CanConvertToSpeckle(object @object)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Base ConvertToSpeckle(object @object)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<Base> ConvertToSpeckle(List<object> objects)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Speckle.Addin/Objects/Objects/BuiltElements/SolidWorks/CustomProperty.cs
@@ -0,0 +1,47 @@
+﻿using Speckle.Core.Models;
+
+namespace Objects.BuiltElements.SolidWorks;
+
+/// <summary>
+/// SolidWorks CustomProperty.
+/// </summary>
+public sealed class CustomProperty: Base
+{
+    public CustomProperty() { }
+
+    public CustomProperty(
+        string name, 
+        string value, 
+        string evaluatedValue = null)
+    {
+        Name = name;
+        Value = value;
+        EvaluatedValue = evaluatedValue;
+    }
+
+    /// <summary>
+    /// Property name.
+    /// </summary>
+    public string Name { get; set; }
+
+    /// <summary>
+    /// Source value.
+    /// </summary>
+    public string Value { get; set; }
+
+    /// <summary>
+    /// Configuration.
+    /// </summary>
+    public string Configuration { get; set; }
+
+    /// <summary>
+    /// Eval Value.
+    /// </summary>
+    public string EvaluatedValue { get; set; }
+
+    ///<inheritdoc/>
+    public override string ToString()
+    {
+        return $"{{{nameof(Name)}:{Name}, {nameof(Value)}:{EvaluatedValue}}}";
+    }
+}
